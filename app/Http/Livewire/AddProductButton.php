cc5edb9c167c75b4d07689fa9be27e4f7df1e8f7
shenter <shenter@shenter.me>
2022-01-03 21:08:22 +0300

    Refactor & livewire component

---
+++ app/Http/Livewire/AddProductButton.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class AddProductButton extends Component
+{
+    public function render()
+    {
+        return view('livewire.add-product-button');
+    }
+}
--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -10,23 +10,45 @@
     <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
 Cart contents
     </div>
-<table>
+<table  class="table table-hover ">
+    <thead>
+    <tr>
+        <th scope="col"></th>
+        <th scope="col">Name</th>
+        <th scope="col">In Cart</th>
+        <th scope="col">+</th>
+
+
+    </tr>
+    </thead>
+    <tbody>
     @foreach($products as $product)
         <tr>
 
             <!-- Product image-->
             <td>
-                <img class="card-img-top" src="images/1.png" alt="..." height="10" width="auto"/>
+                <img class=" img-fluid" src="images/1.png" alt="..." height="5" />
             </td>
             <!-- Product details-->
             <td>{{$product->first()->title}}</td>
-            <td>{{$product->count()}}$</td>
+            <td>{{$product->count()}}</td>
+            <td>
+                <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded">
+                    +
+                </button>
+                <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded">
+                    -
+                </button>
+            </td>
+
 
         </tr>
     @endforeach
+    </tbody>
 </table>
-
+                <button value="Checkout">Checkout fake button</button>
             </div>
+
         </div>
     </section>
 
