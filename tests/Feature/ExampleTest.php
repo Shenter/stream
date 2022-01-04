925a32fb3eb1b51845c6c129938950d4ff6e297d
shenter <shenter@shenter.me>
2022-01-04 13:13:49 +0300

    Added cart test

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -21,7 +21,7 @@ class ExampleTest extends TestCase
     public function test_example()
     {
         $response = $this->get('/');
-        $response->assertStatus(200);
+        $response->assertStatus(302);
     }
     public function test_an_action_that_requires_authentication()
     {
@@ -39,6 +39,16 @@ class ExampleTest extends TestCase
         $response = $this->actingAs($user)
             ->get('product');
         $response->assertSee('test product');
+    }
+
+    public function test_product_can_be_added_to_cart()
+    {
+        $user = User::factory()->create();
+        $product = Product::create(['title'=>'second test product','price'=>110]);
+        Cart::create(['product_id'=>$product->id, 'user_id'=>$user->id]);
+        $response = $this->actingAs($user)
+            ->get('cart');
+        $response->assertSee('second test product');
     }
 
 
